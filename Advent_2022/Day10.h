64caa49296e30869446cb0046a6cf868332c6144
Jeremy Rybicki <jeremyrybicki@gmail.com>
2022-12-21 20:40:45 -0500

    Back from break this is very sloppy but I was rushing

--- Advent_2022/Day10.h
+++ Advent_2022/Day10.h
@@ -8,8 +8,8 @@
 
 int Day10_Main(Star Part_Of_Day)
 {
-    //std::ifstream DataFile("Input_Data//Day10_Input.txt");
-    std::ifstream DataFile("Input_Data//test.txt");
+    std::ifstream DataFile("Input_Data//Day10_Input.txt");
+    //std::ifstream DataFile("Input_Data//test.txt");
     std::string line;
 
     int cycle = 1;
@@ -21,10 +21,11 @@ int Day10_Main(Star Part_Of_Day)
 
     //Rows of 40 pixels
     //Sprite is 3 pixels wide and 1 pixel is drawn per cycle
-    std::vector<std::array<int, 40>> screen;
-
+    std::vector<std::array<char, 40>> screen;
+    int sprInd = 0;
 
     while (getline(DataFile, line)) {
+
         if (line.find("noop") != std::string::npos) {
             temp_cycle = 1;
             temp_x = 0;
@@ -35,14 +36,46 @@ int Day10_Main(Star Part_Of_Day)
         }
 
         for (int i = 0; i < temp_cycle; i++) {
-            if (cycle % 40 == 20) {
+            if (cycle % 40 == 20 && cycle > 0) {
                 sumStr += cycle * x_reg;
             }
+            
+            std::array<char, 40>* curLine;
+
+            //Want this to happen at 0 as well
+            if (((cycle - 1) / 40) >= screen.size()) {
+                std::array<char, 40> line{ ' ' };
+                screen.push_back(line);
+
+                curLine = &screen.back();
+            } else {
+                int index = (cycle - 1) / 40;
+                curLine = &screen[index];
+            }
+
+            char draw = '.';
+            if ((cycle - ((screen.size() - 1) * 40) - x_reg <= 2) && (cycle - ((screen.size() - 1) * 40) - x_reg >= 0)) {
+                draw = '#';
+            }
+
+            curLine->at(cycle - 1 - ((screen.size() - 1) * 40)) = draw;
             cycle++;
         }
+        
         x_reg += temp_x;
+        sprInd = x_reg - 1 - ((screen.size() - 1) * 40);
         int test = 0;
     }
+
+    for (int i = 0; i < screen.size(); i++) {
+        std::array<char, 40> arr = screen[i];
+
+        for (int i = 0; i < 40; i++) {
+            std::cout << arr[i];
+        }
+        std::cout << std::endl;
+    }
+
     DataFile.close();
 
     return sumStr;
